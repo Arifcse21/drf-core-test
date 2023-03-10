0c52e5c2ce5de28d2077eb93f0fcb5c7ff889b45
Arifcse21 <arifcse21@gmail.com>
2023-03-10 11:28:44 +0600

    script updated

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 echo "script started running ..."
-python manage.py flush
 python manage.py makemigrations
 python manage.py migrate
 DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username "$SUPER_USER_NAME" --email "$SUPER_USER_EMAIL" --no-input
